7a7b76c7e3ed0ff979002e151e47535fff16acb1
Kongpon Charanwattanakit <jackykongpon@gmail.com>
2018-10-28 04:49:16 +0700

    Updates

---
+++ precache-manifest.44a5aab5b25f150e045853a327e0276b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a027651b8c493878dc1",
+    "url": "/isellfood/static/js/runtime~main.1a027651.js"
+  },
+  {
+    "revision": "4a38c7fbe7527fb47045",
+    "url": "/isellfood/static/js/main.4a38c7fb.chunk.js"
+  },
+  {
+    "revision": "84ae380c842e6dd28cd1",
+    "url": "/isellfood/static/js/1.84ae380c.chunk.js"
+  },
+  {
+    "revision": "84ae380c842e6dd28cd1",
+    "url": "/isellfood/static/css/1.c5e1c8cd.chunk.css"
+  },
+  {
+    "revision": "99d7cf4235a83da92088fd8c282faae6",
+    "url": "/isellfood/index.html"
+  }
+];
